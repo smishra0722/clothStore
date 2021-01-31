d61256bf95cca2e4c8b2560468231789b1d5614b
smishra0722 <s.mishra0722@gmail.com>
2021-01-31 08:11:23 +0530

    some minor changes

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 export const MenuItem = () => (
-    <div className='menu-item'></div>
+    <div className='menu-item'>
+        
+    </div>
 );
